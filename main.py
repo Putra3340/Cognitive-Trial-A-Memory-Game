796892def1bae926dee6750a4caf92b333f09321
Putra3340 <putraerer3@gmail.com>
2025-11-17 14:15:08 +0700

    Base Game

---
+++ main.py
@@ -0,0 +1,83 @@
+import random
+import os
+import time
+
+# Global Variable
+score = 0
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def get_choices(level):
+    if level == "1":  # Easy
+        return ["↑", "↓", "←", "→"]
+    elif level == "2":  # Normal
+        return [str(i) for i in range(10)]
+    elif level == "3":  # Hard
+        return [chr(i) for i in range(65, 91)]  # A–Z
+    elif level == "4":  # Very Hard
+        return ["cat", "moon", "red", "blue", "star", "note", "time", "fire"]
+    else:
+        return None
+
+
+def validate(answer, user_answer):
+    for i in range(len(answer)):
+        if i >= len(user_answer):
+            return False  # user kurang huruf
+
+        if answer[i] != user_answer[i]:
+            return False  # ada huruf beda
+
+    return True
+
+
+def main():
+    print("=== Cognitive Trial: A Memory Game ===\n\n")
+    print("Pilih tingkat kesulitan:")
+    print("1. Easy (Arah panah)")
+    print("2. Normal (Angka)")
+    print("3. Hard (Huruf)")
+    print("4. Impossible (Kata)")
+
+    level = input("Masukkan pilihan (1-4): ").strip()
+    choices = get_choices(level)
+
+    if choices is None:
+        print("Pilihan tidak valid!")
+        return
+    
+    sequence = []
+
+    while True:
+        # menambah satu item random ke sequence
+        sequence.append(random.choice(choices))
+
+        # tampilkan sequence ke pemain
+        for item in sequence:
+            clear_screen()
+            print("Ingat urutan berikut:")
+            print(item)
+            time.sleep(1)
+
+        # hapus layar
+        clear_screen()
+        print("Masukkan ulang urutan tadi:")
+        answer = input("> ").strip()
+
+        # cek jawaban
+        if (validate("".join(sequence),answer)):
+            score += 1
+            print("Benar! Lanjut ke ronde berikutnya...")
+            time.sleep(1)
+        else:
+            print("\nSalah! Permainan berakhir.")
+            print("Urutan yang benar: ", " ".join(sequence))
+            print("Skor Akhir:", score)
+            input("Tekan Enter untuk kembali ke menu utama")
+            clear_screen()
+            break
+
+if __name__ == "__main__":
+    while True:
+        main()
